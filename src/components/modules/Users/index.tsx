13a267d5c91a9884b81ff6ffafaa12e6a10e1629
Zyrave <rob.rivermark@gmail.com>
2018-12-13 08:38:30 +0700

    Add basic layout

--- src/components/modules/Users/index.tsx
+++ src/components/modules/Users/index.tsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import Layout from '../Layout';
 
 interface IProps {
   fetchRequest(): void;
@@ -7,7 +10,7 @@ interface IProps {
 
 interface IState {}
 
-class index extends Component<IProps, IState> {
+class Index extends Component<IProps, IState> {
   state = {};
 
   componentDidMount() {
@@ -18,27 +21,131 @@ class index extends Component<IProps, IState> {
     const { data } = this.props;
 
     return (
-      <div className="list-group">
-        {data.map((user: any) => (
-          <a
-            href="#"
-            className="list-group-item list-group-item-action flex-column align-items-start"
-            key={user.id}
-          >
-            <div className="d-flex w-100 justify-content-between">
-              <h5 className="mb-1">{user.name}</h5>
-              <small>{user.email}</small>
+      <Layout>
+        <div className="content-wrapper mt-5">
+          <section className="content-header">
+            <div className="container-fluid">
+              <div className="row mb-2">
+                <div className="col-sm-6">
+                  <h1>Users</h1>
+                </div>
+                <div className="col-sm-6">
+                  <ol className="breadcrumb float-sm-right">
+                    <li className="breadcrumb-item">
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li className="breadcrumb-item active">Users</li>
+                  </ol>
+                </div>
+              </div>
             </div>
-            <p className="mb-1">
-              Donec id elit non mi porta gravida at eget metus. Maecenas sed
-              diam eget risus varius blandit.
-            </p>
-            <small>{user.username}</small>
-          </a>
-        ))}
-      </div>
+          </section>
+
+          <section className="content">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-12">
+                  <div className="card">
+                    <div className="card-header">
+                      <h3 className="card-title">Users</h3>
+
+                      <div className="card-tools">
+                        <div
+                          className="input-group input-group-sm"
+                          style={{ width: '150px' }}
+                        >
+                          <input
+                            type="text"
+                            name="table_search"
+                            className="form-control float-right"
+                            placeholder="Search"
+                          />
+
+                          <div className="input-group-append">
+                            <button type="submit" className="btn btn-default">
+                              <i className="fa fa-search" />
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="card-body table-responsive p-0">
+                      <table className="table table-hover">
+                        <tbody>
+                          <tr>
+                            <th>ID</th>
+                            <th>User</th>
+                            <th>Email</th>
+                            <th>Status</th>
+                            <th>Reason</th>
+                            <th>Progress</th>
+                          </tr>
+                          {data.map((user: any) => (
+                            <tr key={user.id}>
+                              <td>{user.id}</td>
+                              <td>{user.name}</td>
+                              <td>{user.email}</td>
+                              <td>
+                                <span className="badge bg-primary">
+                                  Approved
+                                </span>
+                              </td>
+                              <td>
+                                This user was approved by <b>Anil</b>
+                              </td>
+                              <td className="align-middle">
+                                <div className="progress progress-xs">
+                                  <div
+                                    className="progress-bar bg-primary"
+                                    style={{ width: '100%' }}
+                                  />
+                                </div>
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+
+                      <div className="card-footer clearfix">
+                        <ul className="pagination pagination-sm m-0 float-right">
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              &laquo;
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              1
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              2
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              3
+                            </a>
+                          </li>
+                          <li className="page-item">
+                            <a className="page-link" href="#">
+                              &raquo;
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </Layout>
     );
   }
 }
 
-export default index;
+export default Index;
